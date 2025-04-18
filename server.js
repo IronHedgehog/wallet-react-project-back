c8a444cf076ab86023e1f744d3a0f494fb5b3329
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2025-04-18 16:59:30 +0300

    back

--- server.js
+++ server.js
@@ -5,17 +5,29 @@ const authRoutes = require("./routes/auth");
 const transactionRoutes = require("./routes/transactions");
 
 dotenv.config();
+const cors = require("cors");
+
 const app = express();
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://wallet-react-app.netlify.app"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use("/auth", authRoutes);
 app.use("/api/transactions", transactionRoutes);
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => {
-  app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
-  });
-}).catch((err) => console.log(err));
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`Server running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => console.log(err));
