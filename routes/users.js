21c50308c237b3318fc03c051e950e90947e6836
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2025-04-18 17:35:01 +0300

    add authMiddleware

---
+++ routes/users.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const authMiddleware = require("../middleware/auth");
+const User = require("../models/User");
+
+// GET /api/users/current
+router.get("/current", authMiddleware, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    res.json({
+      email: user.email,
+      balance: user.balance,
+    });
+  } catch (err) {
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/users");
 const transactionRoutes = require("./routes/transactions");
 const cors = require("cors");
 
@@ -18,6 +19,7 @@ app.use(
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 app.use("/api/transactions", transactionRoutes);
 
 mongoose
