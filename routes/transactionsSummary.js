f97dc69b6b20a660cee8530b648adb4ca79cc1af
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2025-04-21 13:25:22 +0300

    fix transactionsSummary route

--- routes/transactionsSummary.js
+++ routes/transactionsSummary.js
@@ -50,40 +50,42 @@ router.get("/transactions-summary", authMiddleware, async (req, res) => {
       });
     }
 
-    const startDate = new Date(`${year}-${month}-01`);
-    const endDate = new Date(`${year}-${month}-31`);
+    const paddedMonth = String(month).padStart(2, "0");
 
     const transactions = await Transaction.find({
       owner: userId,
-      date: { $gte: startDate, $lte: endDate },
+      transactionDate: {
+        $gte: new Date(`${year}-${paddedMonth}-01`),
+        $lte: new Date(`${year}-${paddedMonth}-31`),
+      },
     });
 
-    let expenseSummary = 0;
-    let incomeSummary = 0;
-    const categoriesSummaryMap = {};
+    const summary = {};
+    let income = 0;
+    let expenses = 0;
 
-    for (const transaction of transactions) {
-      const { type, amount, category } = transaction;
-
-      if (!categoriesSummaryMap[category]) {
-        categoriesSummaryMap[category] = { name: category, total: 0, type };
+    for (const tx of transactions) {
+      if (!summary[tx.categoryId]) {
+        summary[tx.categoryId] = { total: 0, type: tx.type };
       }
+      summary[tx.categoryId].total += tx.amount;
 
-      categoriesSummaryMap[category].total += amount;
-
-      if (type === "EXPENSE") {
-        expenseSummary += amount;
-      } else if (type === "INCOME") {
-        incomeSummary += amount;
-      }
+      if (tx.type === "INCOME") income += tx.amount;
+      if (tx.type === "EXPENSE") expenses += tx.amount;
     }
 
-    const categoriesSummary = Object.values(categoriesSummaryMap);
+    const categoriesSummary = Object.entries(summary).map(
+      ([name, { total, type }]) => ({
+        name,
+        total,
+        type,
+      })
+    );
 
     res.json({
       categoriesSummary,
-      expenseSummary,
-      incomeSummary,
+      expenseSummary: expenses,
+      incomeSummary: income,
     });
   } catch (error) {
     res.status(500).json({ message: "Server error", error: error.message });
