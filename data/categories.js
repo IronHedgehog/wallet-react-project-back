f8324d64814f0b027412a5b5fd5ee2aa84029f61
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2025-04-21 15:08:49 +0300

    fix categories

--- data/categories.js
+++ data/categories.js
@@ -1,76 +1,76 @@
-const { v4: uuidv4 } = require("uuid");
 const categories = [
   {
-    id: uuidv4(),
+    id: "c1-main-expenses",
     name: "Main expenses 💸",
     type: "EXPENSE",
     backgroundColor: "#FED057",
   },
   {
-    id: uuidv4(),
+    id: "c2-products",
     name: "Products 🛒",
     type: "EXPENSE",
     backgroundColor: "#FFD8D0",
   },
   {
-    id: uuidv4(),
+    id: "c3-car",
     name: "Car 🚗",
     type: "EXPENSE",
     backgroundColor: "#FD9498",
   },
   {
-    id: uuidv4(),
+    id: "c4-self-care",
     name: "Self care 🧴",
     type: "EXPENSE",
     backgroundColor: "#C5BAFF",
   },
   {
-    id: uuidv4(),
+    id: "c5-child-care",
     name: "Child care 👶",
     type: "EXPENSE",
     backgroundColor: "#6E78E8",
   },
   {
-    id: uuidv4(),
+    id: "c6-household",
     name: "Household products 🧼",
     type: "EXPENSE",
     backgroundColor: "#4A56E2",
   },
   {
-    id: uuidv4(),
+    id: "c7-education",
     name: "Education 🎓",
     type: "EXPENSE",
     backgroundColor: "#81E1FF",
   },
   {
-    id: uuidv4(),
+    id: "c8-leisure",
     name: "Leisure 🎮",
     type: "EXPENSE",
     backgroundColor: "#24CCA7",
   },
   {
-    id: uuidv4(),
+    id: "c9-other-expenses",
     name: "Other expenses 🧾",
     type: "EXPENSE",
     backgroundColor: "#00AD84",
   },
   {
-    id: uuidv4(),
+    id: "c10-entertainment",
     name: "Entertainment 🎬",
     type: "EXPENSE",
     backgroundColor: "#FFD8D0",
   },
   {
-    id: uuidv4(),
+    id: "c11-travel",
     name: "Travel ✈️",
     type: "EXPENSE",
     backgroundColor: "#FD9498",
   },
   {
-    id: uuidv4(),
+    id: "c12-income",
     name: "Income 💰",
     type: "INCOME",
     backgroundColor: "#81E1FF",
   },
 ];
+
 module.exports = categories;
